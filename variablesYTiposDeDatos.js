bf60cb9ef5466725cab50c63b4b4888eceb3840c
karen M ulcue L <looshiri1@gmail.com>
2025-07-10 16:12:32 -0500

    He creado las  variables y los datos

---
+++ variablesYTiposDeDatos.js
@@ -0,0 +1,21 @@
+///Declarar variables y usar tipos de datos primitivo 
+// (number, string, boolean, null, undefined).
+//  Ejercicio 1:
+//  •Crea una variable nombre con tu nombre.
+//  •Crea una variable edad con tu edad.
+//  •Crea una variable esEstudiante con true o false.
+//  Ejercicio 2:
+//  •Declara una variable sinValor sin asignarle nada (queda undefined).
+//  •Declara una variable valorNulo y asígnale null.
+//  •Muestra ambas en consola y observa la diferencia
+const prompt  = require ("prompt-sync")();
+let nombre;
+nombre = "Karen Ulcue";
+let estudiante = true;
+let sinValor;
+let valorNulo = null;
+
+console.log(sinValor);
+console.log(valorNulo);
+
+
